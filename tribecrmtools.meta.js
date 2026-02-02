c8d7c560cb67c951a421512e10f657e1fdffdca7
WijZijnGERRIT <servicedesk@wijzijngerrit.nl>
2026-02-02 11:02:03 +0100

    2026.2.2.1

--- tribecrmtools.meta.js
+++ tribecrmtools.meta.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Tribe CRM tools
-// @namespace    https://wijzijngerrit.nl/
+// @namespace    https://gesp.zn-man.nl/
 // @updateURL    https://github.com/WijZijnGERRIT/plugins/raw/refs/heads/tribe/tribecrmtools.meta.js
 // @downloadURL  https://github.com/WijZijnGERRIT/plugins/raw/refs/heads/tribe/tribecrmtools.user.js
-// @version      2026.1.14.1
+// @version      2026.2.2.1
 // @description  Dankzij deze plugin zijn er diverse tools om Tribe een beetje beter te maken. De instellingen en keuzes voor deze tools worden alleen opgeslagen in deze browser sessie en worden niet bewaard in Tribe.
 // @author       Daniel
 // @match        https://app.tribecrm.nl/*
