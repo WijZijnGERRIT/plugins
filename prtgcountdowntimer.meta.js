7a33d3eb86495b564cd359145ff6edad867a808c
WijZijnGERRIT <servicedesk@wijzijngerrit.nl>
2026-02-11 10:12:22 +0100

    2026.2.11.1

---
+++ prtgcountdowntimer.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         PRTG Page numbers and Clock and Enovation status
+// @version      2026.2.11.1
+// @updateURL    https://github.com/WijZijnGERRIT/plugins/raw/refs/heads/prtg/prtgcountdowntimer.meta.js
+// @downloadURL  https://github.com/WijZijnGERRIT/plugins/raw/refs/heads/prtg/prtgcountdowntimer.user.js
+// @description  Add page numbers, a countdown timer and clock (by replacing element <id=pagenumbers> and optionally <id=clock>)), and current Enovation status (OK or ERROR)
+// @author       Daniel
+// @match        https://prtg.ddfr.nl/public/mapshow_simple.htm?id=*
+// @match        https://gesp.zn-man.nl/tools/plugins
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
