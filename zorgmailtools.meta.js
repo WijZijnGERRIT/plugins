6443d500b1c02569a6d7eaa19f0f8ab8087a93c6
WijZijnGERRIT <servicedesk@wijzijngerrit.nl>
2026-02-11 08:48:50 +0100

    2026.2.11.1

---
+++ zorgmailtools.meta.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         ZorgMail tools
+// @namespace    https://gesp.zn-man.nl/
+// @updateURL    https://github.com/WijZijnGERRIT/plugins/raw/refs/heads/zorgmail/zorgmailtools.meta.js
+// @downloadURL  https://github.com/WijZijnGERRIT/plugins/raw/refs/heads/zorgmail/zorgmailtools.user.js
+// @version      2026.2.11.1
+// @description  Diverse ZorgMail gerelateerde tools om het gebruik van Enovation Platform, M.Center, Passage ID en Adresboek allemaal wat makkelijker te maken.
+// @author       Daniel
+// @match        https://adresboek.zorgmail.nl/*
+// @match        https://enovationplatform.com/*
+// @match        https://account.passageid.nl/auth/realms/passageid/login-actions/*
+// @match        https://mcenter.zorgmail.nl/*
+// @match        https://gesp.zn-man.nl/tools/plugins
+// @run-at       document-end
+// @grant        unsafeWindow
+// @grant        GM_listValues
+// @grant        none
+// ==/UserScript==
